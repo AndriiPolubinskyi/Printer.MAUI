16b845952777cf91259e41451f646dde65ad106c
Andrii Polubinskyi <polubin@gmail.com>
2024-05-14 20:58:18 +0300

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Printer.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Printer.MAUI.Helpers;
+
+namespace Printer.MAUI;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void btnPrintPDF_Clicked(object sender, EventArgs e)
+    {
+        HttpClient httpClient = new HttpClient();
+        HttpResponseMessage response = await httpClient.GetAsync(@"https://gbihr.org/images/docs/test.pdf");
+
+        var pdfDocumentStream = await response.Content.ReadAsStreamAsync();
+        var fileName = "test.pdf";
+        var helper = new PrintHelper();
+
+#if ANDROID
+        await helper.PrintAsync(pdfDocumentStream, fileName);
+
+#elif __IOS__ || MACCATALYST
+		helper.Print(pdfDocumentStream, fileName);
+
+#endif
+    }
+
+}
+
+
